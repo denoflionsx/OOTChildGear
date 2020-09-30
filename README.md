e99d1e8e19858c3c63a08ee369c4d0c406d7e766
Denoflions <denvstheworld@gmail.com>
2020-09-30 18:44:45 -0400

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# OOTChildGear
-Use adult equipment as child.
+# OOT Child Gear
+This is an online mod for [ModLoader64](https://github.com/hylian-modding/ModLoader64).
+
+## Requirements
+* Ocarina of Time Online 2.X.
+
+## Features
+* See items that are normally invisible on child Link.
+* Optionally mark all items usable by child Link.
+
+## Building
+1. Install the ModLoader64 SDK.
+2. Clone this repository.
+3. modloader64 -nbd
+
+### Discord
+For further information or assistance please join the [ModLoader64 discord](https://discord.gg/Vb8mKT6).
---
+++ src/ChildGear/ChildGear.ts
@@ -0,0 +1,77 @@
+import { IPlugin, IModLoaderAPI, ModLoaderEvents } from 'modloader64_api/IModLoaderAPI';
+import { IOOTCore } from 'modloader64_api/OOT/OOTAPI';
+import { InjectCore } from 'modloader64_api/CoreInjection';
+import { EventHandler } from 'modloader64_api/EventHandler';
+import { Z64RomTools } from 'Z64Lib/API/Z64RomTools';
+import { Z64LibSupportedGames } from 'Z64Lib/API/Z64LibSupportedGames';
+import { onViUpdate } from 'modloader64_api/PluginLifecycle';
+
+interface ChildGearConfig {
+    unlockItemsAsChild: boolean;
+}
+
+class ChildGear implements IPlugin {
+
+    ModLoader!: IModLoaderAPI;
+    pluginName?: string | undefined;
+    @InjectCore()
+    core!: IOOTCore;
+    kokiri: number = 0;
+    config!: ChildGearConfig;
+
+    preinit(): void {
+        this.config = this.ModLoader.config.registerConfigCategory("ChildGear") as ChildGearConfig;
+        this.ModLoader.config.setData("ChildGear", "unlockItemsAsChild", true);
+    }
+    init(): void {
+    }
+    postinit(): void {
+    }
+
+    onTick(frame?: number | undefined): void {
+        if (!this.core.helper.isTitleScreen() && this.core.helper.isSceneNumberValid()) {
+            if (this.ModLoader.emulator.rdramRead8(0x801DAB6D) === 0x3B && this.kokiri > 0) {
+                this.ModLoader.emulator.rdramWrite32(0x800F790C, this.kokiri);
+                this.ModLoader.emulator.rdramWrite32(0x800F790C + 8, this.kokiri);
+            } else {
+                this.ModLoader.emulator.rdramWrite32(0x800F790C, 0x80837800 + 0x1D690);
+                this.ModLoader.emulator.rdramWrite32(0x800F790C + 8, 0x80837800 + 0x1D690);
+            }
+        }
+    }
+
+    @EventHandler(ModLoaderEvents.ON_ROM_PATCHED)
+    onRomPatched(evt: any) {
+        if (this.config.unlockItemsAsChild) {
+            this.ModLoader.logger.info("Unlocking all items as child. Beware.")
+            let rom: Buffer = evt.rom;
+            let tools: Z64RomTools = new Z64RomTools(this.ModLoader, Z64LibSupportedGames.OCARINA_OF_TIME);
+            let p = tools.decompressDMAFileFromRom(rom, 33);
+            for (let i = 0; i < 0x7F; i++) {
+                p.writeUInt8(0x9, 0x0165B4 + i);
+            }
+            tools.recompressDMAFileIntoRom(rom, 33, p);
+        }
+    }
+
+    @onViUpdate()
+    onVi() {
+        if (this.ModLoader.ImGui.beginMainMenuBar()) {
+            if (this.ModLoader.ImGui.beginMenu("Mods")) {
+                if (this.ModLoader.ImGui.beginMenu("ChildGear")) {
+                    if (this.ModLoader.ImGui.menuItem("All items usable as child (caution)", undefined, this.config.unlockItemsAsChild, true)) {
+                        this.config.unlockItemsAsChild = !this.config.unlockItemsAsChild;
+                        this.ModLoader.config.save();
+                    }
+                    this.ModLoader.ImGui.text("These settings require a game restart to take effect.");
+                    this.ModLoader.ImGui.endMenu();
+                }
+                this.ModLoader.ImGui.endMenu();
+            }
+            this.ModLoader.ImGui.endMainMenuBar();
+        }
+    }
+
+}
+
+module.exports = ChildGear;
